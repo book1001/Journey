02b50b6dc0874513a8a8db25981bd0cc939cea5e
Halim <book1001.db@gmail.com>
2024-06-03 20:04:22 -0400

    GUI

--- public/index2.js
+++ public/index2.js
@@ -14,14 +14,42 @@ document.addEventListener("DOMContentLoaded", function() {
           // Get the content div
           const contentDiv = document.getElementById('content');
 
+          const infoDiv = document.createElement('div');
+          infoDiv.classList.add(`infoItem`);
+          infoDiv.textContent = 'IT’S TRULY WONDERFUL THAT, AMIDST THIS VAST EXPANSE OF EARTH, YOU AND I CAN COME TOGETHER IN THE SAME SPACE AND LISTEN TO THE SAME MUSIC SIMULTANEOUSLY. “COMMUNAL RADIO CLUB” IS A SPACE WHERE VISITORS CAN LISTEN TO LIVE RADIO STATIONS. CLICKING THE BLUE JOG DIAL ☐ IN THE MIDDLE OF THE SCREEN WILL PLAY A RANDOM RADIO STATION WITH AN ODD ANIMATION.';
+
           // Loop through each item and create a div for it
           items.forEach((item, index) => {
+            //   const masonryDiv = document.createElement('div');
+            //   masonryDiv.classList.add(`grid-item`);
+
               const itemDiv = document.createElement('div');
               // Assign a class based on index (for demonstration, cycle through 3 styles)
+              itemDiv.classList.add(`item`);
               itemDiv.classList.add(`item${(index % 3) + 1}`);
-              itemDiv.textContent = item;
+          
+
+              const roofDiv = document.createElement('div');
+              roofDiv.classList.add(`roof${(index % 3) + 1}`);
+              roofDiv.textContent 
+              = `░░░░░░░░░░░░
+              ░░░░░░░░░░░░░░░░░░░░░░░
+              ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░`;
+
+              const wallDiv = document.createElement('div');
+              wallDiv.classList.add(`wall${(index % 3) + 1}`);
+              wallDiv.textContent = '▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█▓▓█';
+
+              const textDiv = document.createElement('div');
+              textDiv.classList.add(`textSign`);
+              textDiv.textContent = item;
+
               contentDiv.appendChild(itemDiv);
+              itemDiv.appendChild(roofDiv);
+              itemDiv.appendChild(wallDiv);
+              itemDiv.appendChild(textDiv);
           });
+          contentDiv.appendChild(infoDiv);
       })
       .catch(error => console.error('Error fetching JSON data:', error));
 });
