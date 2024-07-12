3698cc6ba05eb3a16d2ea1e54c52026d9f8e82c8
ortel.app <ortel.app@gmail.com>
2024-07-13 03:25:35 +0900

    Update uppy.js

--- public/uppy.js
+++ public/uppy.js
@@ -58,6 +58,23 @@ uppy.on('transloadit:result', (stepName, result, assembly) => {
 uppy.on('transloadit:complete', (assembly) => {
   console.log("[complete]");
   console.log(assembly);
+
+  setTimeout(async () => {
+    try {
+      const response = await fetch('/fetch-and-append-data');
+      const data = await response.json();
+      console.log(data);
+
+      const contentDiv = document.getElementById('content');
+      const itemDiv = createElement(data);
+      contentDiv.insertBefore(itemDiv, contentDiv.children[1]);
+
+      document.getElementById('failAlert').style.background = 'blue';
+    } catch (error) {
+      console.error('Failed to fetch and append data:', error);
+      // Handle error scenario
+    }
+  }, 1000); // 0.5초를 밀리초 단위로 지정
 });
 
 // uppy.use(AwsS3, {
@@ -69,22 +86,7 @@ uppy.on('transloadit:complete', (assembly) => {
 uppy.on('complete', async (result) => {
   const successfulUploads = result.successful;
   if (successfulUploads.length > 0) {
-    setTimeout(async () => {
-      try {
-        const response = await fetch('/fetch-and-append-data');
-        const data = await response.json();
-        console.log(data);
-
-        const contentDiv = document.getElementById('content');
-        const itemDiv = createElement(data);
-        contentDiv.insertBefore(itemDiv, contentDiv.children[1]);
-
-        document.getElementById('failAlert').style.background = 'blue';
-      } catch (error) {
-        console.error('Failed to fetch and append data:', error);
-        // Handle error scenario
-      }
-    }, 1000); // 0.5초를 밀리초 단위로 지정
+    
   }
 });
 
