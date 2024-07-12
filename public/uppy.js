64a257af80852a762f5fe5c572553a96319eb45b
ortel.app <ortel.app@gmail.com>
2024-07-13 02:05:52 +0900

    Update uppy.js

--- public/uppy.js
+++ public/uppy.js
@@ -5,93 +5,102 @@ import { Uppy, Dashboard, Webcam, Transloadit } from "https://releases.transload
 // const Dashboard = require("@uppy/dashboard");
 // const Webcam = require("@uppy/webcam");
 
-const uppy = new Uppy()
+const uppy = new Uppy();
 
-  // uppy.addFile({
-  //   name: 'my-file.jpg', // file name
-  //   type: 'image/jpeg', // file type
-  //   data: blob, // file blob
-  // });
+// uppy.addFile({
+//   name: 'my-file.jpg', // file name
+//   type: 'image/jpeg', // file type
+//   data: blob, // file blob
+// });
 
-  uppy.use(Dashboard, { 
-    inline: true, 
-    target: '#uppy',
-    // target: 'body' 
-  });
-  uppy.use(Webcam, { 
-    target: Uppy.Dashboard, 
-    modes: 'picture',
-    mirror: false,
-    mobileNativeCamera: true,
-    videoConstraints: {
-        facingMode: 'environment'
-     }
-  });
-  // uppy.use(XHRUpload, { 
-  //   // endpoint: 'https://journey-qxo4f.ondigitalocean.app/upload',
-  //   endpoint: 'http://localhost:8000/image',
-  //   fieldName: 'photo',
-  //   formData: true
-  // });
-
-  uppy.use(Transloadit, {
-    assemblyOptions: {
-      params: {
-        auth: { key: '2568b8e69bc679351eeb9ae93694482e' },
-        template_id: 'cc8128449fb849729c3620ceefb5f3e8',
-      },
-    },
-  });
-
-  // Optionally listen to events
-  uppy.on('transloadit:assembly-created', (assembly, fileIDs) => {});
-  uppy.on('transloadit:upload', (file, assembly) => {});
-  uppy.on('transloadit:assembly-executing', (assembly) => {});
-  uppy.on('transloadit:result', (stepName, result, assembly) => {});
-  uppy.on('transloadit:complete', (assembly) => {});
-
-  // uppy.use(AwsS3, {
-	// 	shouldUseMultipart: (file) => file.size > 100 * 2 ** 20,
-	// 	companionUrl: 'https://companion.uppy.io',
-  //   limit: 1
-	// });
-  
-  uppy.on('complete', async (result) => {
-    const successfulUploads = result.successful;
-    if (successfulUploads.length > 0) {
-      setTimeout(async () => {
-        try {
-          const response = await fetch('/fetch-and-append-data');
-          const message = await response.text();
-          console.log(message);
-          document.getElementById('failAlert').style.background = 'blue';
-        } catch (error) {
-          console.error('Failed to fetch and append data:', error);
-          // Handle error scenario
-        }
-      }, 3000); // 0.5초를 밀리초 단위로 지정
+uppy.use(Dashboard, { 
+  inline: true, 
+  target: '#uppy',
+  // target: 'body' 
+});
+uppy.use(Webcam, { 
+  target: Uppy.Dashboard, 
+  modes: 'picture',
+  mirror: false,
+  mobileNativeCamera: true,
+  videoConstraints: {
+      facingMode: 'environment'
     }
-  });
+});
+// uppy.use(XHRUpload, { 
+//   // endpoint: 'https://journey-qxo4f.ondigitalocean.app/upload',
+//   endpoint: 'http://localhost:8000/image',
+//   fieldName: 'photo',
+//   formData: true
+// });
 
-  // uppy.on('complete', async (result) => {
-  //   const successfulUploads = result.successful;
-  //   if (successfulUploads.length > 0) {
-  //     try {
-  //       const response = await fetch('/fetch-and-append-data');
-  //       const message = await response.text();
-  //       console.log(message);
-  //     } catch (error) {
-  //       console.error('Failed to fetch and append data:', error);
-  //       // Handle error scenario
-  //     }
-  //   }
-  // });
-  
-  
+uppy.use(Transloadit, {
+  assemblyOptions: {
+    params: {
+      auth: { key: '2568b8e69bc679351eeb9ae93694482e' },
+      template_id: 'cc8128449fb849729c3620ceefb5f3e8',
+    },
+  },
+});
 
-  // uppy.on('complete', (res) => {
-  //   console.log(res.successful);
-  // });
+// Optionally listen to events
+uppy.on('transloadit:assembly-created', (assembly, fileIDs) => {});
+uppy.on('transloadit:upload', (file, assembly) => {});
+uppy.on('transloadit:assembly-executing', (assembly) => {});
+uppy.on('transloadit:result', (stepName, result, assembly) => {});
+uppy.on('transloadit:complete', (assembly) => {});
+
+// uppy.use(AwsS3, {
+// 	shouldUseMultipart: (file) => file.size > 100 * 2 ** 20,
+// 	companionUrl: 'https://companion.uppy.io',
+//   limit: 1
+// });
+
+uppy.on('file-added', async (file) => {
+  const response = await fetch('db.json');
+  const data = await response.json();
+
+  const contentDiv = document.getElementById('content');
+  const itemDiv = createElement(data[data.length - 1]);
+  contentDiv.insertBefore(itemDiv, contentDiv.children[1]);
+});
+
+uppy.on('complete', async (result) => {
+  const successfulUploads = result.successful;
+  if (successfulUploads.length > 0) {
+    setTimeout(async () => {
+      try {
+        const response = await fetch('/fetch-and-append-data');
+        const message = await response.text();
+        console.log(message);
+        document.getElementById('failAlert').style.background = 'blue';
+      } catch (error) {
+        console.error('Failed to fetch and append data:', error);
+        // Handle error scenario
+      }
+    }, 3000); // 0.5초를 밀리초 단위로 지정
+  }
+});
+
+// uppy.on('complete', async (result) => {
+//   const successfulUploads = result.successful;
+//   if (successfulUploads.length > 0) {
+//     try {
+//       const response = await fetch('/fetch-and-append-data');
+//       const message = await response.text();
+//       console.log(message);
+//     } catch (error) {
+//       console.error('Failed to fetch and append data:', error);
+//       // Handle error scenario
+//     }
+//   }
+// });
+
+
+
+// uppy.on('complete', (res) => {
+//   console.log(res.successful);
+// });
 
   
 
